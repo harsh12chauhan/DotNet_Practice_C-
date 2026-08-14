cb82e53b57ff6862ed277581f7733ab9f78225f2
Chauhan Harsh <chauhhar@ap.dtcs.local>
2026-08-14 12:11:34 +0530

    Complete with delegates

---
+++ Coding_02/C#TopicsRevision02/Delegates/ProcessFile.cs
@@ -0,0 +1,45 @@
+﻿namespace Delegates
+{
+    internal static class ProcessFile
+    {
+        // Delegates mini project
+        public delegate void ProcessFilehandler(string File);
+        public static void LogToConsole(string File)
+        {
+            Console.WriteLine($"{File} is logged to Console");
+        }
+
+        public static void SaveToDatabase(string File)
+        {
+
+            Console.WriteLine($"{File} Data saved to database");
+
+        }
+
+        public static void SendEmail(string File)
+        {
+
+            Console.WriteLine($"{File} Email Send");
+
+        }
+        
+        public static void SendSms(string File)
+        {
+
+            Console.WriteLine($"{File} Send via Sms");
+
+        }
+
+        public static void Process(string File, ProcessFilehandler processFileHandler) {
+
+            processFileHandler(File);
+
+        }
+        public static void Process02(string File, Action<string> processFileHandler) {
+
+            processFileHandler(File);
+
+        }
+
+    }
+}
---
+++ Coding_02/C#TopicsRevision02/Delegates/Utility.cs
@@ -0,0 +1,36 @@
+﻿namespace Delegates
+{
+    public static class Utility
+    {
+        public delegate int Operation(int a, int b);
+        public  static int Add(int a, int b) => a + b;
+        public static int Sub(int a, int b) => a - b;
+        public static int Multiply(int a, int b) => a * b;
+        public static void Calculate01(int a, int b, Operation operation)
+        {
+            int result = operation(a, b);
+            Console.WriteLine($"result: {result}");
+        }
+
+        public static void Calculate02(int a, int b, Func<int, int, int> operation)
+        {
+            int result = operation(a, b);
+            Console.WriteLine($"Result: {result}");
+        }
+
+
+        // Multicasting Delegates
+        public static void Email() {
+            Console.WriteLine("Email send");
+        }
+        public static void Sms() {
+            Console.WriteLine("Sms send");
+        }
+        public static void Whatsapp() {
+            Console.WriteLine("Whatsapp message send");
+        }
+
+        public delegate void Notify();      
+
+    }
+}
